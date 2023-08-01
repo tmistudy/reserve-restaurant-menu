c0957029bb06782a45f7a87ff7a3d3119501ff4f
富張達也 <tmi@fuchoutatsuyanoMacBook-Pro.local>
2023-08-01 17:59:14 +0900

    検索機能追加

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,9 +3,8 @@ class RestaurantsController < ApplicationController
   end
 
   def search
-    if params[:area].present?
-      @restaurants = Restaurant.where('area LIKE ?', "%#{params[:area]}%")
-      session[:area] = params[:area]
+    if params[:area_or_menu].present?
+      @restaurants = Restaurant.where('name LIKE(?) OR area LIKE(?) OR address LIKE(?) OR genre LIKE(?)', "%#{params[:area_or_menu]}%","%#{params[:area_or_menu]}%","%#{params[:area_or_menu]}%","%#{params[:area_or_menu]}%")
     else
       @restaurants = Restaurant.all
     end
@@ -14,6 +13,5 @@ class RestaurantsController < ApplicationController
   def show
     @restaurant = Restaurant.find(params[:id])
     @menus = JSON.parse(@restaurant.menu)
-    params[:area] = session[:area]
   end
 end
